d55c49506cc0bcec22ad0fa7395e2c088805d056
Riquelme6789 <jcunalata@alumno.montepiedra.edu.ec>
2025-10-06 13:20:17 -0500

    Add files via upload

---
+++ Un programa que pida el nombre y la edad y los muestre por pantalla.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+    int edad;
+
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+
+    cout << "Nombre: " << nombre << ", Edad: " << edad << endl;
+
+    return 0;
+}
