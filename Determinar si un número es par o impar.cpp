f802e2bd5f714fa2cca1396729034501db936583
Riquelme6789 <jcunalata@alumno.montepiedra.edu.ec>
2025-10-06 13:21:55 -0500

    Add files via upload

---
+++ Determinar si un número es par o impar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Ingrese un número: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << num << " es par." << endl;
+    else
+        cout << num << " es impar." << endl;
+
+    return 0;
+}
